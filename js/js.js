ceca867f050405e4855ccfccf74cef876bbd1766
KuPriv <113209570+KuPriv@users.noreply.github.com>
2022-10-05 17:23:21 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function calc_shaur() {
+  let f1 = document.getElementsByName("field1");
+  let f2 = document.getElementsByName("field2");
+  let r = document.getElementById("result");
+  r.innerHTML = f1[0].value * f2[0].value;
+  return false;
+}
