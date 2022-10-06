306f456083b5937216c144dc920513e0e5e68244
KuPriv <113209570+KuPriv@users.noreply.github.com>
2022-10-06 21:38:13 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,10 @@
-function calc_shaur() {
-  let f1 = document.getElementsByName("field1");
-  let f2 = document.getElementsByName("field2");
-  let r = document.getElementById("result");
-  r.innerHTML = f1[0].value * f2[0].value;
-  return false;
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+  function calc_shaur() {
+    let f1 = document.getElementsByName("field1");
+    let f2 = document.getElementsByName("field2");
+    let r = document.getElementById("result");
+    r.innerHTML = f1[0].value * f2[0].value;
+    return false;
+  }
+});
