a8f63f3a1cbfdb5c657b887665af74df5374d521
KuPriv <113209570+KuPriv@users.noreply.github.com>
2022-10-08 16:16:46 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,10 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("DOM fully loaded and parsed");
-  function calc_shaur() {
-    let f1 = document.getElementsByName("field1");
-    let f2 = document.getElementsByName("field2");
-    let r = document.getElementById("result");
-    r.innerHTML = f1[0].value * f2[0].value;
-    return false;
-  }
-});
+function calc_shaur() {
+  let f1 = document.getElementsByName("field1");
+  let f2 = document.getElementsByName("field2");
+  let r = document.getElementById("result");
+  r.innerHTML = f1[0].value * f2[0].value;
+  return false;
+}
